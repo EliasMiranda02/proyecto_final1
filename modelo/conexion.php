476ae25d02f069cc34e47ed891f0d6e4dfa863a0
Eduardo <vplalo11@gmail.com>
2024-10-24 10:02:22 -0600

    VERSION2.0

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,17 @@
 <?php
-
-    $conexion=new mysqli("localhost", "root", "root", "chiapas_viajero");
-    $conexion->set_charset("utf8");
+    class Conexion extends PDO{
+        private $hostBD='localhost';
+        private $nombreBD='chiapas_viajero';
+        private $usuarioBD='root';
+        private $passwordBD='root';
+        public function __construct(){
+            try {
+                parent::__construct('mysql:host='.$this->hostBD.';dbname='.$this->nombreBD.';charset=utf8',$this->usuarioBD,$this->passwordBD,
+            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            } catch (Exception $e) {
+                exit;
+            }
+            
+        }
+    }
 ?>
