2a1091cdbf9542f3b4e42f58d3dcd6ede65264fd
Eduardo <vplalo11@gmail.com>
2024-10-24 10:06:42 -0600

    VERSION2.2

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,4 +1,16 @@
 <?php
-    $conexion=new mysqli("localhost", "root", "root", "chiapas_viajero");
-    $conexion->set_charset("utf8");
+    //METODO DE CONEXION USANDO PDO SERVIDOR LOCAL
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'chiapas_viajero');
+
+    //AHORA, ESTABLECEMOS LA CONEXIÓN.
+    try{
+        //EJECUTAMOS LAS VARIABLES Y APLICACIONES UT8
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        echo "Conexión Satisfactoria";
+    } catch (PDOException $e){
+        exit("Error: " . $e->getMessage());
+    }
 ?>
