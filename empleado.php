f243be3e4773a8d02fa8fe5d65a7c7a222d5280a
Eduardo <vplalo11@gmail.com>
2024-10-24 10:50:03 -0600

    VERSION2.3.3

--- empleado.php
+++ empleado.php
@@ -9,19 +9,122 @@
 </head>
 
 <body>
-<div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+    <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
 
-  <div class="btn-group" role="group">
-    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-      Cargos
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-      <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-    </ul>
-  </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <div class="btn-group" role="group">
+            <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Cargos
+            </button>
+            <ul class="dropdown-menu">
+                <li>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Administradores">
+                        Launch demo modal
+                    </button>
+                </li>
+                <li>
+
+                </li>
+            </ul>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal" id="Administradores" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <!-- TABLA DE USUARIOS -->
+                        <div class="col-12 p-2">
+                            <table class="table">
+                                <thead class="Bg-info">
+                                    <tr>
+                                        <th scope="col">id_usuario</th>
+                                        <th scope="col">nombre</th>
+                                        <th scope="col">apellido materno</th>
+                                        <th scope="col">apellido paterno</th>
+                                        <th scope="col" class="px-3 text-center">email</th>
+                                        <th scope="col" class="text-center">clave_lada</th>
+                                        <th scope="col">telefono</th>
+                                        <th scope="col">fecha_registro</th>
+                                        <th scope="col">contraseña</th>
+                                        <th scope="col">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    include "modelo/conexion.php";
+                                    include "controlador/eliminar_usuario.php";
+                                    $sql = $conexion->query("select * from clientes");
+                                    while ($datos = $sql->fetch_object()) { ?>
+                                        <tr>
+                                            <th scope="row"><?= $datos->id_cliente ?></th>
+                                            <td><?= $datos->nombre ?></td>
+                                            <td><?= $datos->apellido_materno ?></td>
+                                            <td><?= $datos->apellido_paterno ?></td>
+                                            <td><?= $datos->email ?></td>
+                                            <td><?= $datos->clave_lada ?></td>
+                                            <td><?= $datos->telefono ?></td>
+                                            <td><?= $datos->fecha_registro ?></td>
+                                            <td><?= $datos->contraseña ?></td>
+                                            <td>
+                                                <!-- Botón de edición con ícono y modal -->
+
+                                                <!-- Modal Bootstrap -->
+
+                                                <a href="#" class="btn btn-small btn-danger" data-bs-toggle="modal" data-bs-target="#editModal<?= $datos->id_cliente ?>">
+                                                    <i class="fa-solid fa-trash"></i>
+                                                </a>
+
+
+                                                <div class="modal fade" id="editModal<?= $datos->id_cliente ?>" tabindex="-1" aria-labelledby="editModalLabel<?= $datos->id_cliente ?>" aria-hidden="true">
+                                                    <div class="modal-dialog modal-dialog-centered">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="editModalLabel<?= $datos->id_cliente ?>">Editar Usuario <?= $datos->id_cliente ?></h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <form>
+                                                                    <div class="mb-3">
+                                                                        <label for="nombre" class="form-label">Nombre</label>
+                                                                        <input type="text" class="form-control" id="nombre" value="<?= $datos->nombre ?>">
+                                                                    </div>
+                                                                    <div class="mb-3">
+                                                                        <label for="nombre" class="form-label">Telefono</label>
+                                                                        <input type="text" class="form-control" id="nombre" value="<?= $datos->telefono ?>">
+                                                                    </div>
+
+                                                                    <div class="mb-3">
+                                                                        <label for="email" class="form-label">Email</label>
+                                                                        <input type="email" class="form-control" id="email" value="<?= $datos->email ?>">
+                                                                    </div>
+                                                                    <!-- Agrega más campos según sea necesario -->
+                                                                    <input type="submit" name="eliminar" value="Eliminar">
+                                                                </form>
+
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
