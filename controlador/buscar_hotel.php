4bad4f46dbf14564f834efae9d0114c0a089d382
Eduardo <vplalo11@gmail.com>
2024-10-27 22:06:26 -0600

    VERSION6.3

---
+++ controlador/buscar_hotel.php
@@ -0,0 +1,37 @@
+<?php
+include "../modelo/conexion.php";
+
+$campo = $_POST['campo'];
+$query = $_POST['query'];
+
+// Sanitize input
+$campo = $conexion->real_escape_string($campo);
+$query = $conexion->real_escape_string($query);
+
+// Si query es '%', significa que debe mostrar todos los registros
+$sqlQuery = $query === '%' ? "SELECT * FROM hoteles" : "SELECT * FROM hoteles WHERE $campo LIKE '%$query%'";
+
+$sql = $conexion->query($sqlQuery);
+
+if ($sql->num_rows > 0) {
+    while ($datos = $sql->fetch_object()) {
+        echo "<tr>";
+        echo "<td><input type='checkbox' name='ids[]' value='{$datos->id_hotel}'></td>";
+        echo "<th scope='row'>{$datos->id_hotel}</th>";
+        echo "<td>{$datos->nombre_hotel}</td>";
+        echo "<td>{$datos->direccion}</td>";
+        echo "<td>{$datos->clave_lada}</td>";
+        echo "<td>{$datos->telefono}</td>";
+        echo "<td>{$datos->correo_electronico}</td>";
+        echo "<td>{$datos->numero_habitaciones}</td>";
+        echo "<td>{$datos->descripcion}</td>";
+        echo "<td>{$datos->precio_noche}</td>";
+        echo "<td>{$datos->calificacion}</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='13' class='text-center'>No se encontraron resultados.</td></tr>";
+}
+
+$conexion->close();
+?>
